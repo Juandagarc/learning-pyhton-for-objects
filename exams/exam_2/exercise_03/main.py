cb1268bf34a3e5d65ef58145be52eea8924b154e
Juandagarc <juandagarce@gmail.com>
2024-10-16 21:43:34 -0500

    exam 2

---
+++ exams/exam_2/exercise_03/main.py
@@ -0,0 +1,60 @@
+from modules.shop import Shop, payment
+import json
+
+path = 'data.json'
+
+
+def load_products(path):
+    with open(path, 'r') as file:
+        return json.load(file)
+
+
+def main():
+    data = load_products(path)
+    shop = Shop(data, path)  # We create the shop object
+    while True:
+        print("Que desea hacer?")
+        print("1. Mostrar productos")
+        print("2. Comprar productos")
+        print("3. Agregar productos")
+        print("4. Salir")
+        option = input("Ingrese la opcion: ")
+        if option == "1":
+            shop.show_products()
+        elif option == "2":
+            order_list = []
+            print("Ingrese los productos que desea comprar")
+            while True:
+                name = input("Ingrese el nombre del producto: ")
+                stock = int(input("Ingrese la cantidad: "))
+                order = shop.order_product(name, stock)
+                if order == [0, 0] or order is None:
+                    print("No es posible realizar la compra por falta de cantidad o porque no tenemos el producto")
+                else:
+                    order_list.append(order)
+                print("Desea agregar otro producto? (s/n)")
+                add = input()
+                if add == "n":
+                    break
+            total = payment(order_list)
+            print(f"El total a pagar es: {total}")
+        elif option == "3":
+            print("Ingrese los datos del producto")
+            print("Categorias: Electronic, Food, Clothes")
+            category = input("Ingrese la categoria: ")
+            name = input("Ingrese el nombre: ")
+            price = float(input("Ingrese el precio: "))
+            stock = int(input("Ingrese el stock: "))
+            shop.add_product(category, name, price, stock)
+        elif option == "4":
+            print("Guardando productos...")
+            shop.save_products()
+            print("Productos guardados")
+            print("Saliendo...")
+            break
+        else:
+            print("Opcion invalida")
+
+
+if __name__ == "__main__":
+    main()
---
+++ exams/exam_2/exercise_03/modules/shop.py
@@ -0,0 +1,79 @@
+import json
+from .product import Electronic, Food, Clothes
+
+
+def payment(order_list):
+    total = 0
+    for order in order_list:
+        if order is None:
+            continue
+        elif order[1] >= 5:
+            total += order[0] * order[1] * 0.8
+        else:
+            total += order[0] * order[1]
+    return total
+
+
+class Shop:
+    def __init__(self, data, path):
+        self.__data = data
+        self.__path = path
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, data):
+        self.__data = data
+
+    @property
+    def path(self):
+        return self.__path
+
+    @path.setter
+    def path(self, path):
+        self.__path = path
+
+    def add_product(self, category, name, price, stock):
+        if category == "Electronic":
+            warranty = input("Ingrese la garantia: ")
+            product = Electronic(name, price, stock, warranty)
+        elif category == "Food":
+            expiration_date = input("Ingrese la fecha de vencimiento: ")
+            product = Food(name, price, stock, expiration_date)
+        elif category == "Clothes":
+            size = input("Ingrese la talla: ")
+            product = Clothes(name, price, stock, size)
+        if category in self.data:
+            self.data[category].append(product.json_return())
+        else:
+            print("Categoria no válida")
+
+    def show_products(self):
+        for category in self.data:
+            print(f"Categoria: {category}")
+            for product in self.data[category]:
+                print(f"\tNombre: {product['name']}")
+                print(f"\tPrecio: {product['price']}")
+                print(f"\tStock: {product['stock']}")
+                if category == "Electronic":
+                    print(f"\tGarantia: {product['warranty']}")
+                elif category == "Food":
+                    print(f"\tFecha de vencimiento: {product['expiration_date']}")
+                elif category == "Clothes":
+                    print(f"\tTalla: {product['size']}")
+
+    def order_product(self, name, stock):
+        for category in self.data:
+            for product in self.data[category]:
+                if product['name'] == name:
+                    if product['stock'] >= stock:
+                        product['stock'] -= stock
+                        return [product['price'], stock]
+                    else:
+                        return [0, 0]
+
+    def save_products(self):
+        with open(self.path, "w") as file:
+            json.dump(self.data, file, indent=4)
